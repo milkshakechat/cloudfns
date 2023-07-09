f3ec858b7e76fa22aa4fa7d55287d69cc71f08d4
0xterran <0xterran@protonmail.com>
2023-07-10 04:14:14 +0700

    merchant registrations

--- firebase/functions/src/functions/createUserFirestore.ts
+++ firebase/functions/src/functions/createUserFirestore.ts
@@ -3,9 +3,12 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 admin.initializeApp();
 import {
+  FirestoreCollection,
   UserID,
   User_Firestore,
   Username,
+  WalletID,
+  Wallet_Firestore,
   defaultThemeColorHex,
   genderEnum,
   localeEnum,
@@ -13,6 +16,7 @@ import {
 } from "@milkshakechat/helpers";
 import { generateAvailablePlaceholderNames } from "../utils/username";
 import { createFirestoreTimestamp } from "../services/firestore";
+import { v4 as uuidv4 } from "uuid";
 
 export const createUserFirestore = functions.auth
   .user()
@@ -22,6 +26,7 @@ export const createUserFirestore = functions.auth
       const { username, displayName } =
         await generateAvailablePlaceholderNames();
       const now = createFirestoreTimestamp();
+      const walletID = uuidv4();
       const newUser: User_Firestore = {
         id: user.uid as UserID,
         username: username as Username,
@@ -41,10 +46,24 @@ export const createUserFirestore = functions.auth
         gender: genderEnum.other,
         interestedIn: [],
         usernameLastUpdated: now,
+        mainWalletID: walletID as WalletID,
+      };
+      const newWallet: Wallet_Firestore = {
+        id: walletID as WalletID,
+        ownerID: user.uid as UserID,
+        hasMerchantPrivilege: false,
+        title: `Main Wallet for User ${user.uid}`,
+        createdAt: now,
+        note: "Created automatically upon user creation.",
+        cookieBalance: 0,
       };
       const db = admin.firestore();
-      await db.collection("users").doc(user.uid).set(newUser);
+      await Promise.all([
+        db.collection(FirestoreCollection.USERS).doc(user.uid).set(newUser),
+        db.collection(FirestoreCollection.WALLETS).doc(user.uid).set(newWallet),
+      ]);
       logger.log("User document written with ID: ", user.uid);
+      logger.log("Wallet document written with ID: ", walletID);
     } catch (e) {
       console.error("Error adding document: ", e);
     }
